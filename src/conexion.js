926d3763425b994fb5df094a0b68de44c074051b
Osman <osman_suastegui@hotmail.com>
2022-11-15 08:14:18 -0700

    coenxion bd

--- src/conexion.js
+++ src/conexion.js
@@ -1,22 +1,21 @@
 import sql from 'mssql'
-const dbsettings  = {
-    user:"tnt-admin",
-    password:"papas123.",
-    server:"servidor-tnt.database.windows.net",
-    database:'pokepoke',
-    options: {
-        trustedConnection: true,
-        encrypt: true,
-        trustServerCertificate: true,        
-      }
+
+const dbsettings = {
+  server: "servidor-tnt.database.windows.net",
+  database: 'pokepoke',
+  user: 'tnt-admin',
+  password: 'papas123.',
+  options: {
+    encrypt: true,
+  }
 }
 
-export const getconnection = async () =>{
-    try {
-      const pool = await sql.connect(dbsettings);  
-      return pool;
-    } catch (error) {
-      console.log(error.message);
-    }
+export const getconnection = async () => {
+  try {
+    const pool = await sql.connect(dbsettings);
+    return pool;
+  } catch (error) {
+    console.log(error.message);
   }
-export {sql}
+}
+export { sql }
--- src/rutas.js
+++ src/rutas.js
@@ -1,22 +1,21 @@
 
-import express from 'express';
-import {getconnection,sql} from './conexion.js'
+import express, { response } from 'express';
+import { getconnection } from './conexion.js'
+import { Request } from 'tedious';
 let router = express.Router()
 
 
-router.get("/",  (req,res) => {
+router.get("/", (req, res) => {
     res.send("hola como estas waa123")
 })
 
 
-router.get('/obtenerPokemones',  async (req, res) => {
+router.get('/obtenerPokemones',async (req, res) => {
     try {
+        // console.log(obtenerPokemones())
         const pool = await getconnection();
-        const result = await pool.request().query(`SELECT * FROM pokemones`)
-
-
-        res.json(result.recordset)
-
+        const result = await pool.request().query('SELECT * FROM pokemones')
+        res.send(result.recordset)
     } catch (error) {
         res.send(error.message)
     }
