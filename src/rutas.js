947c244a4ab12d6668b0ddad1b5267c938032099
Osman <osman_suastegui@hotmail.com>
2022-11-14 21:38:22 -0700

    conexion a la bd

--- src/rutas.js
+++ src/rutas.js
@@ -1,10 +1,24 @@
 
 import express from 'express';
+import {getconnection,sql} from './conexion.js'
 let router = express.Router()
 
 
-router.get("/",(req,res) => {
+router.get("/",  (req,res) => {
     res.send("hola como estas waa123")
 })
 
+
+router.get('/obtenerPokemones', async (req, res) => {
+    try {
+        const pool = await getconnection();
+        const result = await pool.request().query(`SELECT * FROM pokemones`)
+
+
+        res.json(result.recordset)
+
+    } catch (error) {
+        res.send(error.message)
+    }
+})
 export default router
