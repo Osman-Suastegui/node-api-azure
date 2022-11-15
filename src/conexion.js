3812521ec3d7ae4210ef71e2c7e181bd5571695d
Osman <osman_suastegui@hotmail.com>
2022-11-14 23:24:04 -0700

    conexion a bd

--- src/conexion.js
+++ src/conexion.js
@@ -1,22 +1,22 @@
-// import sql from 'mssql'
-// const dbsettings  = {
-//     user:"tnt-admin",
-//     password:"papas123.",
-//     server:"servidor-tnt.database.windows.net",
-//     database:'pokepoke',
-//     options: {
-//         trustedConnection: true,
-//         encrypt: true,
-//         trustServerCertificate: true,        
-//       }
-// }
+import sql from 'mssql'
+const dbsettings  = {
+    user:"tnt-admin",
+    password:"papas123.",
+    server:"servidor-tnt.database.windows.net",
+    database:'pokepoke',
+    options: {
+        trustedConnection: true,
+        encrypt: true,
+        trustServerCertificate: true,        
+      }
+}
 
-// export const getconnection = async () =>{
-//     try {
-//       const pool = await sql.connect(dbsettings);  
-//       return pool;
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//   }
-// export {sql}
+export const getconnection = async () =>{
+    try {
+      const pool = await sql.connect(dbsettings);  
+      return pool;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+export {sql}
--- src/rutas.js
+++ src/rutas.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-// import {getconnection,sql} from './conexion.js'
+import {getconnection,sql} from './conexion.js'
 let router = express.Router()
 
 
@@ -9,17 +9,16 @@ router.get("/",  (req,res) => {
 })
 
 
-router.get('/obtenerPokemones',  (req, res) => {
-    res.send("m123")
-    // try {
-    //     const pool = await getconnection();
-    //     const result = await pool.request().query(`SELECT * FROM pokemones`)
+router.get('/obtenerPokemones',  async (req, res) => {
+    try {
+        const pool = await getconnection();
+        const result = await pool.request().query(`SELECT * FROM pokemones`)
 
 
-    //     res.json(result.recordset)
+        res.json(result.recordset)
 
-    // } catch (error) {
-    //     res.send(error.message)
-    // }
+    } catch (error) {
+        res.send(error.message)
+    }
 })
 export default router
